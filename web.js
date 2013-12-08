4b090d3e2f4ee2cfe4c0b8c01c4dc6306135cdcb
user <eyloncw@gmail.com>
2013-12-08 16:25:23 +0000

    updated web.js

--- web.js
+++ web.js
@@ -1,14 +1,13 @@
 var express = require('express');
 var fs = require('fs');
+var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) {
-  var buffer = new Buffer();
-  response.send(fs.readFileSync("index.html").toString());
+app.get('/', function(request, response) { 
+response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 5000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+app.listen(port, function() {  
+console.log("Listening on " + port);
 });
