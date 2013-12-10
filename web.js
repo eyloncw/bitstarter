2bf7bc8d790a31166086776ffe8e17b576f94a1a
Ubuntu <@gmail.com>
2013-12-10 07:40:45 +0000

    edited wonder.html

--- web.js
+++ web.js
@@ -1,17 +1,13 @@
 var express = require('express');
 var fs = require('fs');
-var htmlfile = "index.html";
+var buffer = new Buffer(fs.readFileSync('index.html'), 'utf-8');
 
 var app = express.createServer(express.logger());
-
-app.get('/', function(request, response) { 
-var html = fs.readfilesync(htmlfile).tostring();
-response.send(html);
-
-
+app.get('/', function(request, response) {
+response.send(buffer.toString());
 });
 
 var port = process.env.PORT || 8080;
-app.listen(port, function() {  
+app.listen(port, function() {
 console.log("Listening on " + port);
 });
